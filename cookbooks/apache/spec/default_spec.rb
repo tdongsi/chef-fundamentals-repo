4f9f12c362dc4b812526f16a93c3e1650f707d11
tdongsi <dongsi.tuecuong@gmail.com>
2015-07-02 19:16:18 -0700

    Add coverage report for ChefSpec. Add test for template.

--- cookbooks/apache/spec/default_spec.rb
+++ cookbooks/apache/spec/default_spec.rb
@@ -1,9 +1,15 @@
 require 'chefspec'
 
+at_exit { ChefSpec::Coverage.report! }
+
 describe 'apache::default' do
-  chef_run = ChefSpec::ServerRunner.new.converge('apache::default')
+  let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
   
   it 'installs apache2' do
     expect(chef_run).to install_package('httpd')
+  end
+  
+  it 'creates clowns.conf' do
+    expect(chef_run).to create_template('/etc/httpd/conf.d/clowns.conf')
   end
 end
